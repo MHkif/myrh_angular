b53d2a5ce08eaf4ade6a63ce1292036f3f51a488
e.elhjoujy <e.elhjoujy@labelvie.ma>
2024-01-07 15:17:15 +0100

    [ ] FIX MERGE PROBLEMS

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,23 @@
-
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { RegisterComponent } from './auth/register/register.component';
-import { HomeComponent } from './home/home.component';
-import { OfferFormComponent } from './offer/offer-form/offer-form.component';
-import { LoginComponent } from './auth/login/login.component';
-import { AuthGuard } from './service/auth.guard';
-import { OfferDetailComponent } from './offer/offer-detail/offer-detail.component';
-import { AccountValidationComponent } from './account-validation/account-validation.component';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {RegisterComponent} from './auth/register/register.component';
+import {HomeComponent} from './home/home.component';
+import {OfferFormComponent} from './offer/offer-form/offer-form.component';
+import {LoginComponent} from './auth/login/login.component';
+import {AuthGuard} from './service/auth.guard';
+import {OfferDetailComponent} from './offer/offer-detail/offer-detail.component';
 import {AdminDashboardComponent} from "./features/admin/dashboard/admin-dashboard.component";
 
 const routes: Routes = [
   {
-    path: 'company/auth/register', component: RegisterComponent, canActivate: [AuthGuard],
+    path: 'company/auth/register', component: RegisterComponent,
+    // canActivate: [AuthGuard],
   },
   {
-    path: 'company/auth/login', component: LoginComponent, canActivate: [AuthGuard],
+    path: 'company/auth/login', component: LoginComponent,
+    // canActivate: [AuthGuard],
 
   },
-
   {
     path: 'admin/auth/login', component: LoginComponent,
   },
@@ -28,27 +27,17 @@ const routes: Routes = [
   {
     path: 'admin/home', component: LoginComponent,
   },
-
   {
-
-    path: 'offers',
-    component: HomeComponent,
-    // canActivate: [AuthGuard],
-
     path: 'offers', component: HomeComponent,
-
+    // canActivate: [AuthGuard],
   },
   {
     path: '', redirectTo: 'offers', pathMatch: 'full',
   },
   {
-
     path: 'offers/new',
     component: OfferFormComponent,
     // canActivate: [AuthGuard],
-
-    path: 'offers/new', component: OfferFormComponent,
-
   },
   {
     path: 'offers/:id', component: OfferDetailComponent,
