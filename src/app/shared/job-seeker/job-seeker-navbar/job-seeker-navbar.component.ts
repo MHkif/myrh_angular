7d872b69f347254fb13d10d97286223766583c2b
e.elhjoujy <e.elhjoujy@labelvie.ma>
2024-01-11 12:00:29 +0100

    [FEATURE NOTIFICATION POPUP  ][ socket service et view   PDR-39-en-temps-reel-a-loffre-et-suivre-ses-candidatures-vue-approuvee-ou-rejetee ]

--- src/app/shared/job-seeker/job-seeker-navbar/job-seeker-navbar.component.ts
+++ src/app/shared/job-seeker/job-seeker-navbar/job-seeker-navbar.component.ts
@@ -2,45 +2,53 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 import {
   JobSeekerApplicationSocketConfigService
 } from "../../../service/jobSeeker/job-application-config/job-seeker-application-socket-config.service";
-import {Subject, takeUntil} from "rxjs";
+import {JobApplicantRes} from "../../../model/JobApplicantRes.model";
 
 @Component({
   selector: 'app-job-seeker-navbar',
   templateUrl: './job-seeker-navbar.component.html',
   styleUrls: ['./job-seeker-navbar.component.css']
 })
-export class JobSeekerNavbarComponent implements OnInit , OnDestroy{
+export class JobSeekerNavbarComponent implements OnInit, OnDestroy {
 
-    notifications: any[] = [];
-    newNotification: boolean = false;
-    jobSeekerId: number = 1;
-    constructor(
-      private jobSeekerApplicationSocketService: JobSeekerApplicationSocketConfigService
-    ) {
-    }
+  notifications: any[] = [];
+  newNotification: boolean = false;
+  jobSeekerId: number = 1;
+  newNotificationObject!:any;
 
+  constructor(
+    private jobSeekerApplicationSocketService: JobSeekerApplicationSocketConfigService
+  ) {
+  }
 
 
   ngOnInit(): void {
-      this.jobSeekerApplicationSocketService.
-      subscribeToNotification("/topic/job_seeker/", this.jobSeekerId, () => {});
+    this.jobSeekerApplicationSocketService.subscribeToNotification("/topic/job_seeker/", this.jobSeekerId, () => {
+    });
 
-      this.jobSeekerApplicationSocketService.notificationsSubject.subscribe((notification) => {
-        console.log("notificationsSubject")
-        console.log(notification)
-        this.notifications.push(notification);
+    this.jobSeekerApplicationSocketService.notificationsSubject.subscribe((notification) => {
+      console.log("notificationsSubject")
+      console.log(notification)
 
-        this.newNotification = true;
-        setTimeout(() => {
-          this.newNotification = false;
-        }, 5000);
+      this.newNotificationObject = notification as JobApplicantRes;
+      console.log(this.newNotificationObject.offer.title)
+      this.notifications.push(this.newNotificationObject);
 
-      });
-    }
+
+
+
+      this.newNotification = true;
+      setTimeout(() => {
+        this.newNotification = false;
+      }, 5000);
+
+    });
+  }
 
   ngOnDestroy(): void {
     this.jobSeekerApplicationSocketService.notificationsSubject.unsubscribe();
 
   }
 
+  protected readonly Date = Date;
 }
